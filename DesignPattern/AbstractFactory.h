a705549479bc147727959a9cfa0ceb53ae107ee3
唐韬 <inserlan@163.com>
2018-12-16 11:47:44 +0800

    修改工厂方法和抽象工厂

--- DesignPattern/AbstractFactory.h
+++ DesignPattern/AbstractFactory.h
@@ -1,185 +1,208 @@
 #pragma once
 #include <iostream>
 
-namespace DP_AF
+// 创建型：抽象工厂（超级工厂）
+// 意图：提供一个创建一系列相关或相互依赖对象的接口，而无需指定它们具体的类。
+// 主要解决：主要解决接口选择的问题。
+// 何时使用：系统的产品有多于一个的产品族，而系统只消费其中某一族的产品。
+// 如何解决：在一个产品族里面，定义多个产品。
+// 关键代码：在一个工厂里聚合多个同类产品。
+// 应用实例：工作了，为了参加一些聚会，肯定有两套或多套衣服吧，比如说有商务装（成套，一系列具体产品）、
+//		时尚装（成套，一系列具体产品），甚至对于一个家庭来说，可能有商务女装、商务男装、时尚女装、时尚男装，
+//		这些也都是成套的，即一系列具体产品。假设一种情况（现实中是不存在的，要不然，没法进入共产主义了，
+//		但有利于说明抽象工厂模式），在您的家中，某一个衣柜（具体工厂）只能存放某一种这样的衣服（成套，一系列具体产品），
+//		每次拿这种成套的衣服时也自然要从这个衣柜中取出了。用OO的思想去理解，所有的衣柜（具体工厂）都是衣柜类的（抽象工厂）某一个，
+//		而每一件成套的衣服又包括具体的上衣（某一具体产品），裤子（某一具体产品），这些具体的上衣其实也都是上衣（抽象产品），
+//		具体的裤子也都是裤子（另一个抽象产品）。
+// 优点：当一个产品族中的多个对象被设计成一起工作时，它能保证客户端始终只使用同一个产品族中的对象。
+// 缺点：产品族扩展非常困难，要增加一个系列的某一产品，既要在抽象的 Creator 里加代码，又要在具体的里面加代码。
+// 注意事项：产品族难扩展，产品等级易扩展。
+
+// Abstract base product
+class Widget
 {
-	// 创建型：抽象工厂（超级工厂）
-	// 意图：提供一个创建一系列相关或相互依赖对象的接口，而无需指定它们具体的类。
-	// 主要解决：主要解决接口选择的问题。
-	// 何时使用：系统的产品有多于一个的产品族，而系统只消费其中某一族的产品。
-	// 如何解决：在一个产品族里面，定义多个产品。
-	// 关键代码：在一个工厂里聚合多个同类产品。
-	// 应用实例：工作了，为了参加一些聚会，肯定有两套或多套衣服吧，比如说有商务装（成套，一系列具体产品）、
-	//		时尚装（成套，一系列具体产品），甚至对于一个家庭来说，可能有商务女装、商务男装、时尚女装、时尚男装，
-	//		这些也都是成套的，即一系列具体产品。假设一种情况（现实中是不存在的，要不然，没法进入共产主义了，
-	//		但有利于说明抽象工厂模式），在您的家中，某一个衣柜（具体工厂）只能存放某一种这样的衣服（成套，一系列具体产品），
-	//		每次拿这种成套的衣服时也自然要从这个衣柜中取出了。用OO的思想去理解，所有的衣柜（具体工厂）都是衣柜类的（抽象工厂）某一个，
-	//		而每一件成套的衣服又包括具体的上衣（某一具体产品），裤子（某一具体产品），这些具体的上衣其实也都是上衣（抽象产品），
-	//		具体的裤子也都是裤子（另一个抽象产品）。
-	// 优点：当一个产品族中的多个对象被设计成一起工作时，它能保证客户端始终只使用同一个产品族中的对象。
-	// 缺点：产品族扩展非常困难，要增加一个系列的某一产品，既要在抽象的 Creator 里加代码，又要在具体的里面加代码。
-	// 注意事项：产品族难扩展，产品等级易扩展。
+public:
+	virtual void draw() = 0;
+};
 
-	// 形状抽象父类
-	class Shape
+// Concrete product family 1
+class LinuxButton : public Widget
+{
+public:
+	void draw() override
 	{
-	public:
-		virtual void Draw() = 0;
-	};
-
-	// 形状实体子类1
-	class Rectangle : public Shape
+		std::cout << "LinuxButton\n";
+	}
+};
+class LinuxMenu : public Widget
+{
+public:
+	void draw() override
 	{
-	public:
-		void Draw() override
-		{
-			std::cout << "Inside Rectangle::Draw() method." << std::endl;
-		}
-	};
+		std::cout << "LinuxMenu\n";
+	}
+};
 
-	// 形状实体子类2
-	class Square : public Shape
+// Concrete product family 2
+class WindowsButton : public Widget
+{
+public:
+	void draw() override
 	{
-	public:
-		void Draw() override
-		{
-			std::cout << "Inside Square::Draw() method." << std::endl;
-		}
-	};
-
-	// 形状实体子类3
-	class Circle : public Shape
+		std::cout << "WindowsButton\n";
+	}
+};
+class WindowsMenu : public Widget
+{
+public:
+	void draw() override
 	{
-	public:
-		void Draw() override
-		{
-			std::cout << "Inside Circle::Draw() method." << std::endl;
-		}
-	};
+		std::cout << "WindowsMenu\n";
+	}
+};
 
-	// 颜色抽象父类
-	class Color
+// before ==================================================
+/**
+ * Here's a client, which uses concrete products directly.
+ * It's code filled up with nasty switch statements
+ * which check the product type before its use.
+ */
+class ClientBefore
+{
+public:
+	void draw()
 	{
-	public:
-		virtual void Fill() = 0;
-	};
+#ifdef LINUX
+		Widget *w = new LinuxButton;
+#else
+		Widget *w = new WindowsButton;
+#endif // LINUX
+		w->draw();
+		display_window_one();
+		display_window_two();
+	}
 
-	// 颜色实体子类1
-	class Red : public Color
+	void display_window_one()
 	{
-	public:
-		void Fill() override
-		{
-			std::cout << "Inside Red::Fill() method." << std::endl;
-		}
-	};
-
-	// 颜色实体子类2
-	class Green : public Color
+#ifdef LINUX
+		Widget *w[] = { new LinuxButton, new LinuxMenu };
+#else
+		Widget *w[] = { new WindowsButton, new WindowsMenu };
+#endif // LINUX
+		w[0]->draw();
+		w[1]->draw();
+	}
+	void display_window_two()
 	{
-	public:
-		void Fill() override
-		{
-			std::cout << "Inside Green::Fill() method." << std::endl;
-		}
-	};
+#ifdef LINUX
+		Widget *w[] = { new LinuxMenu, new LinuxButton };
+#else
+		Widget *w[] = { new WindowsMenu, new WindowsButton };
+#endif // LINUX
+		w[0]->draw();
+		w[1]->draw();
+	}
+};
 
-	// 颜色实体子类3
-	class Blue : public Color
+void AbstractFactoryBefore()
+{
+	ClientBefore *c = new ClientBefore();
+	c->draw();
+	delete c;
+}
+
+// after ==================================================
+// Abstract factory defines methods to create all related products
+class Factory
+{
+public:
+	virtual Widget *create_button() = 0;
+	virtual Widget *create_menu() = 0;
+};
+
+/**
+ * Each concrete factory corresponds to one product
+ * family. It creates all possible products of
+ * one kind.
+ */
+class LinuxFactory : public Factory {
+public:
+	Widget *create_button() {
+		return new LinuxButton;
+	}
+	Widget *create_menu() {
+		return new LinuxMenu;
+	}
+};
+
+/**
+ * Concrete factory creates concrete products, but
+ * returns them as abstract.
+ */
+class WindowsFactory : public Factory {
+public:
+	Widget *create_button() {
+		return new WindowsButton;
+	}
+	Widget *create_menu() {
+		return new WindowsMenu;
+	}
+};
+
+/**
+ * Client receives a factory object from its creator.
+ *
+ * All clients work with factories through abstract
+ * interface. They don't know concrete classes of
+ * factories. Because of this, you can interchange
+ * concrete factories without breaking clients.
+ *
+ * Clients don't know the concrete classes of created
+ * products either, since abstract factory methods
+ * returns abstract products.
+ */
+class ClientAfter
+{
+private:
+	Factory* factory;
+
+public:
+	ClientAfter(Factory* f)
 	{
-	public:
-		void Fill() override
-		{
-			std::cout << "Inside Blue::Fill() method." << std::endl;
-		}
-	};
+		factory = f;
+	}
 
-	// 工厂抽象父类
-	class AbstractFactory
+	void draw()
 	{
-	public:
-		virtual Shape* MakeShape(std::string shapeType) = 0;
-		virtual Color* MakeColor(std::string colorType) = 0;
-	};
+		Widget* w = factory->create_button();
+		w->draw();
+		display_window_one();
+		display_window_two();
+	}
 
-	// 工厂形状实体子类
-	class ShapeFactory : public AbstractFactory
+	void display_window_one()
 	{
-	public:
-		Shape* MakeShape(std::string shapeType) override
-		{
-			if (shapeType == "Rectangle")
-			{
-				return new Rectangle();
-			}
-			else if (shapeType == "Square")
-			{
-				return new Square();
-			}
-			else if (shapeType == "Circle")
-			{
-				return new Circle();
-			}
-			else
-			{
-				return nullptr;
-			}
-		}
+		Widget* w[] = { factory->create_button(), factory->create_menu() };
+		w[0]->draw();
+		w[1]->draw();
+	}
 
-		Color* MakeColor(std::string colorType) override
-		{
-			return nullptr;
-		}
-	};
-
-	// 工厂颜色实体子类
-	class ColorFactory : public AbstractFactory
+	void display_window_two()
 	{
-	public:
-		Shape* MakeShape(std::string shapeType) override
-		{
-			return nullptr;
-		}
+		Widget* w[] = { factory->create_menu(), factory->create_button() };
+		w[0]->draw();
+		w[1]->draw();
+	}
+};
 
-		Color* MakeColor(std::string colorType) override
-		{
-			if (colorType == "Red")
-			{
-				return new Red();
-			}
-			else if (colorType == "Green")
-			{
-				return new Green();
-			}
-			else if (colorType == "Blue")
-			{
-				return new Blue();
-			}
-			else
-			{
-				return nullptr;
-			}
-		}
-	};
-
-	// 创建一个工厂创造器/生成器类，通过传递形状或颜色信息来获取工厂
-	class FactoryProducer
-	{
-	public:
-		static AbstractFactory* MakeFactory(std::string factoryType)
-		{
-			if (factoryType == "Shape")
-			{
-				return new ShapeFactory();
-			}
-			else if (factoryType == "Color")
-			{
-				return new ColorFactory();
-			}
-			else
-			{
-				return nullptr;
-			}
-		}
-	};
+void AbstractFactoryAfter()
+{
+	Factory* factory;
+#ifdef LINUX
+	factory = new LinuxFactory;
+#else
+	factory = new WindowsFactory;
+#endif // LINUX
+	ClientAfter* c = new ClientAfter(factory);
+	c->draw();
 }
